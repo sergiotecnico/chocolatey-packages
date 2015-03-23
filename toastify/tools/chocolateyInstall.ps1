51775bb17c5b057047c504a1d184adb63d40436e
Jerome Cheng <jerome@ayulin.net>
2015-03-23 09:46:40 +0800

    Update to Toastify 1.7.2.

--- toastify/tools/chocolateyInstall.ps1
+++ toastify/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName = 'toastify' # arbitrary name for the package, used in messages
 $installerType = 'EXE' #only one of these: exe, msi, msu
-$url = 'https://toastify.codeplex.com/downloads/get/960495' # download url
-$url64 = 'https://toastify.codeplex.com/downloads/get/960495' # 64bit URL here or remove - if installer decides, then use $url
+$url = 'https://toastify.codeplex.com/downloads/get/1440002' # download url
+$url64 = 'https://toastify.codeplex.com/downloads/get/1440002' # 64bit URL here or remove - if installer decides, then use $url
 $silentArgs = '/S' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
