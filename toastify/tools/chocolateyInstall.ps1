2ab03ebb6a61a6c61be9eca39768a1ecb80f5fd5
sergiotecnico <70968650+sergiotecnico@users.noreply.github.com>
2020-09-09 09:54:58 -0300

    Added download checksum

--- toastify/tools/chocolateyInstall.ps1
+++ toastify/tools/chocolateyInstall.ps1
@@ -1,7 +1,9 @@
-﻿$packageName = 'toastify'
+$packageName = 'toastify'
 $installerType = 'EXE' 
 $url = 'https://github.com/aleab/toastify/releases/download/v1.11.2/ToastifyInstaller.exe' 
-$url64 = 'https://github.com/aleab/toastify/releases/download/v1.11.2/ToastifyInstaller.exe'
+$checksum = '1701E58BB19FA3381A7692BEF7AAA91982637106DE974339B55F1E3868F4C6DC'
+$checksumType = 'sha256'
+$
 $silentArgs = '/S' 
 $validExitCodes = @(0)
 
